b8fc0119c85f35e190025b220e6c788738e19393
Andrew Shaffer <andrew@citizenstable.co>
2021-05-26 15:38:48 -0700

    gempagev2.js

---
+++ pages/gempagev2.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+!function(n){var r={};function o(t){if(r[t])return r[t].exports;var e=r[t]={i:t,l:!1,exports:{}};return n[t].call(e.exports,e,e.exports,o),e.l=!0,e.exports}o.m=n,o.c=r,o.d=function(t,e,n){o.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:n})},o.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},o.t=function(e,t){if(1&t&&(e=o(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(o.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)o.d(n,r,function(t){return e[t]}.bind(null,r));return n},o.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return o.d(e,"a",e),e},o.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},o.p="/",o(o.s=8)}({8:function(t,e,n){"use strict";var o;window.jQuery&&(window.GemQuery=window.jQuery),window.GEMVENDOR&&(o=window.GEMVENDOR);var r={jqueryLoaded:!1},i={};window.__gemStoreData=r,window.__gemStoreFunc=i;var a=(u.prototype.dispatch=function(t,e){(function(t,e){for(var n=Object(i)[t]||[],r=0;r<n.length;r++)(0,n[r])(e)})(t,Object(r)[t]=e)},u.prototype.getState=function(t){return Object(r)[t]},u.prototype.subscribe=function(t,e){Object(i)[t]=Object(i)[t]||[],Object(i)[t].push(e)},u);function u(){}var l=(d.prototype.init=function(){var t="https://web.archive.org/web/20210309065624/https://d1um8515vdn9kb.cloudfront.net/files/jquery191.min.js",e=this;void 0===window.GemQuery?this.ls(t,function(){window.GemQuery=window.jQuery,e.onComplete()}):this.cjv(window.GemQuery.fn.jquery,"1.7.0")<=0?this.ls(t,function(){window.GemQuery=window.jQuery.noConflict(!0),e.onComplete()}):e.onComplete()},d.prototype.onComplete=function(){var e=this;this.ls("https://web.archive.org/web/20210309065624/https://d1um8515vdn9kb.cloudfront.net/files/vendor.js?v=3.0",function(){});var i=o.getLibs(),a=[],u="";if(0<window.GemQuery('[data-label="(P) Meta"]').length&&window.GemQuery('[data-label="(P) Meta"]').each(function(){var t=window.GemQuery(this),e=t.find("script.libs");if(0<e.length){var n;try{n=window.GemQuery.trim(e.html());var r=window.GemQuery.parseJSON(n);if(null!=r.libs)for(var o=0;o<r.libs.length;o++)-1===window.GemQuery.inArray(r.libs[o],i)&&a.push(r.libs[o])}catch(t){console.log(t.message)}}t=t.find("script.script");u+=t.html()}),0==(i=a.concat(i)).length)e.addMetaScripts(u);else if(1==i.length)e.ls(i[0],function(){e.addMetaScripts(u)});else{for(var n=i.length-1,t=0;t<n;t++)this.listLoaded[i[t]]=!1;for(var r=this,t=0;t<n;t++)!function(t){r.ls(i[t],function(){e.listLoaded[i[t]]=!0,e.checkLoadedAll()&&e.ls(i[n],function(){e.addMetaScripts(u)})})}(t)}},d.prototype.checkLoadedAll=function(){for(var t in this.listLoaded)if(!this.listLoaded[t])return!1;return!0},d.prototype.addMetaScripts=function(t){0<window.GemQuery('[data-label="(P) Meta"]').length&&window.GemQuery('<script type="text/javascript">'+t+"<\/script>").insertAfter(window.GemQuery(".gf-script:last"))},d.prototype.cjv=function(t,e){if(t===e)return 0;for(var n=t.split("."),r=e.split("."),o=Math.min(n.length,r.length),i=0;i<o;i++){if(parseInt(n[i])>parseInt(r[i]))return 1;if(parseInt(n[i])<parseInt(r[i]))return-1}return n.length>r.length?1:n.length<r.length?-1:0},d.prototype.ls=function(t,e){var n=document.createElement("script");n.type="text/javascript",n.readyState?n.onreadystatechange=function(){"loaded"!=n.readyState&&"complete"!=n.readyState||(n.onreadystatechange=null,e&&e())}:(n.onload=function(){e&&e()},n.onerror=function(){e&&e()}),n.src=t,document.getElementsByTagName("head")[0].appendChild(n)},d);function d(){this.listLoaded=[],this.init()}window.GEMSTORE&&0!==Object.keys(window.GEMSTORE).length||(window.GEMSTORE=new a),new l}});
+
+}
+/*
+     FILE ARCHIVED ON 06:56:24 Mar 09, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 21:20:10 May 26, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1561.877
+  exclusion.robots: 0.237
+  exclusion.robots.policy: 0.229
+  RedisCDXSource: 1.62
+  esindex: 0.01
+  LoadShardBlock: 1497.565 (3)
+  PetaboxLoader3.datanode: 101.943 (4)
+  CDXLines.iter: 27.98 (3)
+  load_resource: 200.13
+  PetaboxLoader3.resolve: 155.297
+*/
